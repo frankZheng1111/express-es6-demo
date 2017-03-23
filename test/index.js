7e4b3576c7f80f9920b4010c28a193c393fa08c9
frankZheng1111 <DingYuan.Zheng1111@hotmail.com>
2017-03-23 22:11:11 +0800

    Add test

---
+++ test/index.js
@@ -0,0 +1,17 @@
+import http from 'http';
+import assert from 'mocha';
+import { should as _should } from 'chai'
+
+let should = _should();
+
+import '../src/index.js';
+
+describe('Example Node Server', () => {
+  it('should return 200', done => {
+    http.get('http://127.0.0.1:3000', res => {
+      res.statusCode.should.equal(200);
+      done();
+    });
+  });
+});
+
